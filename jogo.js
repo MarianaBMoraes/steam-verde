2b7f53f1b18c462f703233501d868e426700cab6
Mariana <marianabmoraes.ef@gmail.com>
2024-07-24 11:22:57 -0300

    feat: add crud e código completo

--- jogo.js
+++ jogo.js
@@ -1,13 +1,31 @@
 const prompt = require('prompt-sync')();
+
 const jogos = [];
+
+const lerIndice = mensagem => parseInt(prompt(mensagem))
+
+const nomeInvalido = nome => nome == ""
+
+const indiceInvalido = indice => indice < 0 || indice >= jogos.length || isNaN(indice)
+
+
+const listagem = () => jogos.forEach((jogo, i) => { 
+    let sequencia
+    if(jogo.sequencia != -1) {
+        sequencia = jogos[jogo.sequencia].nome
+    } else {
+        sequencia = "Não possui sequência"
+    }
+    console.log(`${i + 1} - ${jogo.nome} - ${jogo.ano} - ${jogo.duracao} - ${jogo.preco} - ${jogo.estudio} - ${sequencia}`)
+})
 
 const modelo = () => {
 
-    let jogo
+    let jogo = {} // não posso adicionar atributos em algo indefinido
 
     while(true) {
         jogo.nome = prompt("Qual é o nome do jogo? ");
-        if(jogo.nome == "") {
+        if(nomeInvalido(jogo.nome)) {
             console.log("O nome não pode ser vazio");
         } else {
             break
@@ -15,7 +33,7 @@ const modelo = () => {
     }
 
     while(true) {
-        jogo.ano = Number(prompt("Qual é o ano do jogo? "));
+        jogo.ano = parseInt(prompt("Qual é o ano do jogo? "));
         if(jogo.ano < 1958 || jogo.ano > 2024 || isNaN(jogo.ano)) {
             console.log("O ano é inválido");
         } else {
@@ -43,7 +61,7 @@ const modelo = () => {
 
     while(true) {
         jogo.estudio = prompt("Qual é o estudio do jogo? ");
-        if(jogo.estudio == "") {
+        if(nomeInvalido(jogo.estudio)) {
             console.log("O estudio não pode ser vazio");
         } else {
             break
@@ -53,11 +71,13 @@ const modelo = () => {
     while(true) {
 
         if(jogos.length == 0) {
-            jogo.sequencia = undefined
+            jogo.sequencia = -1
             break
         }
 
-        jogo.sequencia = parseInt(prompt("Qual é a sequencia do jogo? "));
+        listagem()
+
+        jogo.sequencia = lerIndice("Qual é a sequencia do jogo? Digite 0 caso não houver sequencia") - 1;
 
         // jogos.forEach((el, i) => {
         //     if(el.nome == jogo.sequencia) {
@@ -66,7 +86,7 @@ const modelo = () => {
         //     }
         // })
 
-        if(jogo.sequencia < 0 || jogo.sequencia >= jogos.length || isNaN(jogo.sequencia)) {
+        if(jogo.sequencia != -1 && indiceInvalido(jogo.sequencia)) {
             console.log("A sequencia é invalida");
         } else {
             break
@@ -77,17 +97,14 @@ const modelo = () => {
 }
 const criar = () => {
 
-    let jogo = modelo()
+    const jogo = modelo()
 
     jogos.push(jogo)
 
     console.log("Jogo criado com sucesso")
 }
 
-const listagem = () => jogos.forEach((jogo, i) => console.log(`${i + 1} - ${jogo.nome} - ${jogo.ano} - ${jogo.duracao} - ${jogo.preco} - ${jogo.estudio} - ${jogo.sequencia}`))
-
 const atualizar = () => {
-
 
     while(true) {
 
@@ -96,9 +113,11 @@ const atualizar = () => {
             break
         }
 
-        let indice = parseInt(prompt("Qual é o indice do jogo que deseja atualizar? "))
+        listagem()
 
-        if(indice < 0 || indice >= jogos.length || isNaN(indice)) {
+        const indice = lerIndice("Qual é o indice do jogo que deseja atualizar? ") - 1
+
+        if(indiceInvalido(indice)) {
             console.log("Indice inválido")
         } else {
             const jogo = modelo()
@@ -107,8 +126,35 @@ const atualizar = () => {
         }
     }
 
-    
+}
+
+const remover = () => {
+
+    while(true) {
+
+        listagem()
+
+        const indice = lerIndice("Qual é o indice do jogo que deseja remover? ") - 1
+
+        if(indiceInvalido(indice)) {
+            console.log("Indice inválido")
+        } else {
+            jogos.forEach(jogo => {
+                if(jogo.sequencia == indice) {
+                    jogo.sequencia = -1
+                }
+            })
+            jogos.splice(indice, 1)
+            console.log("Jogo removido com sucesso")
+            break
+        }
+    }
 
 }
 
-
+module.exports = {
+    criar,
+    atualizar,
+    remover,
+    listagem
+}
